3bb68b2b2f7b9c8b1de35862178f7db35034cb87
Jaskirat <jaskiratr@gmail.com>
2017-06-11 13:35:54 -0400

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -15,8 +15,8 @@ document.onmousemove = handleMouseMove
 window.addEventListener('resize', onWindowResize, false)
 
 var stats = new Stats()
-stats.showPanel(1) // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom)
+// stats.showPanel(1) // 0: fps, 1: ms, 2: mb, 3+: custom
+// document.body.appendChild(stats.dom)
 
 var renderer = new THREE.WebGLRenderer({
 	canvas: bgAnimation,
